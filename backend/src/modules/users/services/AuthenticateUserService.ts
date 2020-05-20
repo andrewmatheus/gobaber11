f0c09499a051a4d7f85480092ced4545f2529945
Andrew Matheus <andrewmatheus@gmail.com>
2020-05-20 16:27:30 -0300

    Tipagem

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -48,7 +48,7 @@ class AuthenticateUserService {
 
     const { secret, expiresIn } = authConfig.jwt;
 
-    const token = sign({}, secret, {
+    const token = sign({}, secret || '', {
       subject: user.id,
       expiresIn,
     });
