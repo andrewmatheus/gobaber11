8aad2159abbf253af9d9909e3153eb90d11d51ce
Andrew Matheus <andrewmatheus@gmail.com>
2020-05-22 21:39:02 -0300

    Amazon SES adaptador, mantido o Ethereal para enviar email em desenvolvimento

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,3 +1,5 @@
+require('dotenv/config');
+
 export default {
   jwt: {
     secret: process.env.APP_SECRET,
--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -18,9 +20,10 @@ container.registerSingleton<IMailTemplateProvider>(
   'MailTemplateProvider',
   HandlebarsMailTemplateProvider,
 );
-// registerSingleton não estava utilizando o constructor da class então usou uma instance para poder funcionar continuando sendo um sigleton
-// resolve que vai fazer a injeçao de dependencia
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
