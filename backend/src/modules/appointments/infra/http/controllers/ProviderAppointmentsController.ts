0619761201dae0dd9e12959a0e1a8f66d9825b65
Andrew Matheus <andrewmatheus@gmail.com>
2020-05-25 15:40:53 -0300

    Atualizações para atender regra de negócio

--- backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -2,21 +2,22 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listAppointment = container.resolve(ListProviderAppointmentsService);
 
     const appointments = await listAppointment.execute({
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
       provider_id,
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
@@ -48,7 +49,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
