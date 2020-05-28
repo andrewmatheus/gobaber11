4533f4ee101dae66e8940d0c76e8f414e5407ede
Andrew Matheus <andrewmatheus@gmail.com>
2020-05-28 20:40:57 -0300

    #FrontEnd: Commit Dashboard conectando api e carregando agendamentos de acordo com data/hora

--- backend/src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ backend/src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -24,7 +24,7 @@ export default function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = verify(token, authConfig.jwt.secret);
+    const decoded = verify(token, authConfig.jwt.secret || '');
 
     const { sub } = decoded as ITokenPayload;
 
--- frontend/src/hooks/AuthContext.tsx
+++ frontend/src/hooks/AuthContext.tsx
@@ -1,20 +1,26 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
-}
-
-interface AuthState {
-  token: string;
-  user: object;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -25,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -41,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
