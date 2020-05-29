e640ca12f6f85e3ec8b33b1bbfb602ef380afefa
Andrew Matheus <andrewmatheus@gmail.com>
2020-05-29 11:06:01 -0300

    Finalizado perfil da aplicação web;

--- frontend/src/hooks/ToastContext.tsx
+++ frontend/src/hooks/ToastContext.tsx
@@ -11,7 +11,7 @@ export interface ToastMessage {
   id: string;
   type?: 'success' | 'error' | 'info';
   title: string;
-  description: string;
+  description?: string;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
